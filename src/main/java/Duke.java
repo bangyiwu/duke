71732d72b97b823b28e3858eb53ef559e3c4f45f
Bangyi Wu <bangyi98@gmail.com>
2020-06-30 17:15:45 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,41 @@
+package main.java;
+
+import main.java.Task;
+
 import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> todo = new ArrayList<String>();
+        ArrayList<Task> todo = new ArrayList<Task>();
         System.out.println("What's new scooby doo?\n" + "How can I help you today?");
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
+        while (sc.hasNext()) {
+            String command = sc.next();
             switch(command) {
                 case "bye":
                     System.out.println("See you later alligator ");
                     System. exit(0);
                     break;
                 case "list":
-                    int index = 1;
-                    for ( String str:
+                    for ( Task task:
                             todo) {
-                        System.out.println( index + ". " + str);
-                        index ++;
+                        System.out.println( todo.indexOf(task)+1 + ". " + task.getStatusIcon() + " " + task.description);
                     }
                     if (todo.size() == 0) {
                         System.out.println("Your life is empty now bruh");
                     }
                     break;
+                case "done":
+                    int taskID = sc.nextInt() - 1;
+                    Task task = todo.get(taskID);
+                    task.markAsDone();
+                    System.out.println("Gratz, you finished this dawg :");
+                    System.out.println(task.getStatusIcon()+ task.description);
+                    break;
                 default:
-                    todo.add(command);
-                    System.out.println("okay you need to: " + command);
+                    String fullTask = command + sc.nextLine();
+                    todo.add(new Task(fullTask));
+                    System.out.println("okay you need to: " + fullTask);
             }
         }
     }
